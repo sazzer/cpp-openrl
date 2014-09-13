2d4410e3166ca088c0ec1954fe9f0c8623f80df6
Graham Cox <graham@grahamcox.co.uk>
2014-09-13 09:33:01 +0100

    Split out the test runner so that multiple tests work

--- src/lib/test/sample_test.cpp
+++ src/lib/test/sample_test.cpp
@@ -7,9 +7,8 @@ go_bandit([](){
         it("Can do maths", [](){
             AssertThat(1 + 1, Equals(2));
         });
+        it("Can't do maths", [](){
+            AssertThat(1 + 1, Equals(3));
+        });
     });
 });
-
-int main(int argc, char** argv) {
-    return bandit::run(argc, argv);
-}
