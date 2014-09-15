7777b6c2bd03f00ed3b5b95db2f25533752bdf4d
Graham Cox <graham.cox@oclc.org>
2014-09-15 12:50:14 +0100

    Removed the deliberately broken test

--- src/lib/test/sample_test.cpp
+++ src/lib/test/sample_test.cpp
@@ -7,8 +7,5 @@ go_bandit([](){
         it("Can do maths", [](){
             AssertThat(1 + 1, Equals(2));
         });
-        it("Can't do maths", [](){
-            AssertThat(1 + 1, Equals(3));
-        });
     });
 });
