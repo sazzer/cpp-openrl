59fb2c1ee7989fb6a01713eee44feeb02a732b4e
Graham Cox <graham.cox@oclc.org>
2014-09-17 13:19:21 +0100

    C++ Safe way to open and close a C FILE

--- src/lib/integration/render_map.cpp
+++ src/lib/integration/render_map.cpp
@@ -1,9 +1,26 @@
 #include "world/overview.h"
 #include <string>
 #include <png.h>
+#include <iostream>
+#include <memory>
+
+void fileCloser(FILE* file) {
+        fclose(file);
+        std::cerr << "Closing file: " << file << std::endl;    
+}
+
+std::unique_ptr<FILE, void(*)(FILE*)> openFile(const std::string& filename, const std::string& mode) {
+    FILE* fp = fopen(filename.c_str(), mode.c_str());
+    std::cerr << "Opened file: " << fp << std::endl;
+    return std::unique_ptr<FILE, void(*)(FILE*)>(fp, fileCloser);
+}
 
 void renderMapToFile(const World::Overview& map, const std::string& filename) {
-    
+    auto fp = openFile(filename, "wb");
+    if (!fp) {
+        std::cerr << "Could not open file for writing: " << filename << std::endl;
+        return;
+    }
 }
 
 /**
